0460594d53e2b99627dc2f15ba6c95e0aa8a5a77
jorgebarcelos <jorgebarcelos.faria@gmail.com>
2024-12-25 22:28:32 -0300

    refactor: using chi instead pat for routers

--- pkg/routes/endpoints.go
+++ pkg/routes/endpoints.go
@@ -2,17 +2,17 @@ package routes
 
 import (
 	"net/http"
-
-	"github.com/bmizerany/pat"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
 	"github.com/jorgebarcelos/GoWebApp/pkg/config"
 	"github.com/jorgebarcelos/GoWebApp/pkg/handlers"
 )
 
 func Routes(app *config.AppConfig) http.Handler {
-	mux := pat.New()
+	mux := chi.NewRouter()
 
-	mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
-	mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
-
+	mux.Use(middleware.Recoverer)
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
 	return mux
 }
